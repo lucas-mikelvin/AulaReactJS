7a543b4a2513c487ff15cc100735d9577f64b5c0
lucas-mikelvin <lucas.matos@bandtec.com.br>
2021-04-07 21:42:52 -0300

    Aula 3

---
+++ music-box/src/pages/MinhasMusicas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MinhasMusicas()
+{
+    return (
+        <>
+        <h1>
+            TESTE DE H1
+        </h1>
+        </>
+    );
+}
+
+export default MinhasMusicas;
